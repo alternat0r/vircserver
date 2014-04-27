a130bdf784783cb1c4201f22e92502cc786cdb39
alternat0r <alternator99@gmail.com>
2014-04-28 00:01:38 +0800

    Add MOTD() function;

--- vircserver/modIRCD.vb
+++ vircserver/modIRCD.vb
@@ -9,6 +9,8 @@
     Public ServerTime As String
     Public intMax As Integer
 
+    Public SendMOTD(0 To 50) As String
+
     Sub OP(channel As String, nickname As String, index As Integer)
         frmMain.tcpServer.Send(":" & "ADMIN!general@services.friends.nuclearbomb.com MODE " & channel & "+o " & nickname & vbCrLf)
     End Sub
@@ -17,5 +19,14 @@
         If LCase(target) = LCase(nickname(index)) Then
             frmMain.tcpServer.Send(":" & UCase(target) & Chr(32) & "MODE" & Chr(32) & UCase(target) & " :" & modes & vbCrLf)
         End If
+    End Sub
+
+    Sub MOTD(nickname As String, index As Integer)
+        frmMain.tcpServer.Send(":" & ServerName & " 375 " & nickname & " :- " & ServerName & " Message of the Day -" & vbCrLf)
+        Dim i As Integer
+        For i = 1 To 50
+            frmMain.tcpServer.Send(":" & ServerName & " 372 " & nickname & " :-" & SendMOTD(i) & vbCrLf)
+        Next i
+        frmMain.tcpServer.Send(":" & ServerName & " 376 " & nickname & " :End of Message of the Day" & vbCrLf)
     End Sub
 End Module
